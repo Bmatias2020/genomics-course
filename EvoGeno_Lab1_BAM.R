ee8e3246635ee6b7273d52d56afb1127e0b05863
Bmatias2020 <bmatias@umass.edu>
2020-09-07 13:24:42 +0000

    Updated Names

---
+++ EvoGeno_Lab1_BAM.R
@@ -0,0 +1,42 @@
+### Exercise 1 
+
+``` {r ex1}
+
+x <- 3
+y <- 15
+x+y
+
+y-x
+
+x*y 
+
+y/x
+
+```
+
+### Exercise 2
+
+``` {r ex2}
+x <- 2
+y <- 3
+x^5 > y^4
+
+```
+
+### Exercise 3
+
+``` {r ex3}
+vec<-c(22,62,148,43,129)
+a<-sum(vec)
+vec/a
+
+```
+
+### Exercise 4
+
+``` {r ex4}
+nuc<- "A,T,C,G"
+sort(nuc)
+
+```
+
